668926a2c54b10ed835d2eb8b73d5474cdbda719
meghanachitturi5678 <102587671+meghanachitturi5678@users.noreply.github.com>
2023-06-22 21:08:59 +0530

    Add files via upload

---
+++ q1/q1/output.py
@@ -0,0 +1,14 @@
+def output(ca,m,n):
+    # printing the required grid on terminal obtained by applying rule to initial state for given number of iterations
+   for i in range(m):
+        for j in range(n):
+            print(ca[i][j], end="")
+        print()
+
+    # printing the required grid in output.txt file obtained by applying rule to initial state for given number of iterations
+   with open("./q1/output.txt", "w") as output_file:
+        for i in range(m):
+            for j in range(n):
+                print(ca[i][j], end="", file=output_file)
+            print(end='\n', file=output_file)
+   output_file.close()
